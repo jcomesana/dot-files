d2a357823efe7c014299cf4a8c2875d630b39bd1
Juan Comesaña <jcomesana@gmail.com>
2021-11-15 23:03:48 +0100

    nvim, update LSP diagnostics options

--- .config/nvim/lua/pluggins.rc.lua
+++ .config/nvim/lua/pluggins.rc.lua
@@ -45,6 +45,12 @@ local border = {
 local on_attach = function(client, bufnr)
   vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border})
   vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border})
+  vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+  })
 
   require "lsp_signature".on_attach()
 
