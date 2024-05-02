5097d786599605e61fbd27bdbaab0ed640c49527
Juan Comesaña <jcomesana@gmail.com>
2024-05-02 23:03:51 +0200

    nvim, added configuration for telescope to work with multiple selections

--- .config_lua_neovim/nvim/init.lua
+++ .config_lua_neovim/nvim/init.lua
@@ -798,6 +798,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.lnum ~= nil then
+        vim.cmd(string.format('edit +%s %s', j.lnum, j.path))
+      else
+        vim.cmd(string.format('edit %s', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 require('telescope').setup {
   defaults = {
     -- configure to use ripgrep
@@ -845,6 +863,8 @@ require('telescope').setup {
         ['<LeftMouse>'] = require('telescope.actions').select_default,
         ['<ScrollWheelDown>'] = require('telescope.actions').move_selection_next,
         ['<ScrollWheelUp>'] = require('telescope.actions').move_selection_previous,
+        ['<C-o>'] = function(prompt_bufnr) require('telescope.actions').add_selected_to_qflist(prompt_bufnr) vim.cmd('copen') vim.cmd.cfirst() end,
+        ['<CR>'] = select_one_or_multi,
       },
     },
     layout_config = {
