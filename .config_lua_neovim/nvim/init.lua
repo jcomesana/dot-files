7e209ff7155f38e91bfafc7307118372e1d61159
Juan Comesaña <jcomesana@gmail.com>
2024-10-03 00:06:20 +0200

    nvim, added lazygit

--- .config_lua_neovim/nvim/init.lua
+++ .config_lua_neovim/nvim/init.lua
@@ -155,6 +155,27 @@ require('lazy').setup({
     },
   },
 
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<Leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' }
+    }
+  },
+
   -- Detect tabstop and shiftwidth automatically
   {
     'tpope/vim-sleuth',
