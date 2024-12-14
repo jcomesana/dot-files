e9e745b1d5fd7c17d3a2d2aa66b38bdd51354b80
Juan Comesaña <jcomesana@gmail.com>
2024-12-14 23:42:46 +0100

    nvim, tweaked Snacks.dashboard

--- .config_lua_neovim/nvim/init.lua
+++ .config_lua_neovim/nvim/init.lua
@@ -119,8 +119,7 @@ require('lazy').setup({
         },
         sections = {
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1, limit = 10 },
           { section = "startup" },
         },
       },
