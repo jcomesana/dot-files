a822de69e263a82121f7b4610aed404d183fab3a
Juan Comesaña <jcomesana@gmail.com>
2024-12-28 00:08:25 +0100

    nvim, do not pin version of npm-groovy-lint, exclude colorscheme retrobox

--- .config_lua_neovim/nvim/init.lua
+++ .config_lua_neovim/nvim/init.lua
@@ -1504,7 +1504,7 @@ end
 
 local lsp_servers_handled_with_mason = {}
 if (not is_termux) then
-  table.insert(lsp_servers_handled_with_mason, { "npm-groovy-lint", version = "14.6.0" })
+  table.insert(lsp_servers_handled_with_mason, "npm-groovy-lint")
 end
 
 for server_name, server_config in pairs(lsp_servers) do
@@ -1706,6 +1706,7 @@ local function select_colorscheme()
     "pablo",
     "peachpuff",
     "quiet",
+    "retrobox",
     "ron",
     "shine",
     "slate",
