1bb3195e4026b27c9415bdc1d17e5fafad29e343
Juan Comesaña <jcomesana@gmail.com>
2025-12-28 23:22:09 +0100

    config!: nvim, updated treesitter configuration

--- .config_lua_neovim/nvim/init.lua
+++ .config_lua_neovim/nvim/init.lua
@@ -770,14 +770,71 @@ require("lazy").setup({
   {
     -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      -- Provide context based on treesitter
-      "nvim-treesitter/nvim-treesitter-context",
-      -- Indentation engine
-      "yioneko/nvim-yati",
-    },
+    lazy = false,
     build = ":TSUpdate",
+    branch = "main",
+    config = function()
+      ---@param buf integer
+      ---@param language string
+      local function treesitter_try_attach(buf, language)
+        -- check if parser exists and load it
+        if not vim.treesitter.language.add(language) then
+          return
+        end
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        -- enables treesitter based folds
+        -- for more info on folds see `:help folds`
+        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+        -- enables treesitter based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+
+      local available_parsers = require('nvim-treesitter').get_available()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then
+            return
+          end
+
+          local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+          if vim.tbl_contains(installed_parsers, language) then
+            -- enable the parser if it is installed
+            treesitter_try_attach(buf, language)
+          elseif vim.tbl_contains(available_parsers, language) then
+            -- if a parser is available in `nvim-treesitter` enable it after ensuring it is installed
+            require('nvim-treesitter').install(language):await(function()
+              treesitter_try_attach(buf, language)
+            end)
+          else
+            -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+            treesitter_try_attach(buf, language)
+          end
+        end,
+      })
+
+      -- ensure basic parser are installed
+      local parsers = { "c", "cmake", "cpp", "dockerfile", "groovy", "html", "java", "json",
+                        "kotlin", "lua", "markdown", "markdown_inline", "python", "regex",
+                        "rust", "swift", "toml", "vimdoc", "vim", "xml", "yaml", "yang" }
+      require('nvim-treesitter').install(parsers)
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    lazy = false,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
   },
 
   {
@@ -1185,82 +1242,6 @@ vim.keymap.set("n", "<Leader>ft", require("fzf-lua").treesitter, { desc = "[T]re
 vim.keymap.set("n", "<Leader>fll", require("fzf-lua").loclist, { desc = "[L]oc[L]ist" })
 vim.keymap.set("n", "<Leader>fq", require("fzf-lua").quickfix, { desc = "[Q]uickfix" })
 vim.keymap.set("n", "<Leader>fM", require("fzf-lua").manpages, { desc = "[M]an" })
-
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
--- Defer Treesitter setup after first render to improve startup time of "nvim {filename}"
-vim.defer_fn(function()
----@diagnostic disable-next-line: missing-fields
-  require("nvim-treesitter.configs").setup {
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { "c", "cmake", "cpp", "dockerfile", "groovy", "html", "java", "json",
-                         "kotlin", "lua", "markdown", "markdown_inline", "python", "regex",
-                         "rust", "swift", "toml", "vimdoc", "vim", "xml", "yaml", "yang" },
-
-    auto_install = false,
-    sync_install = false,
-    ignore_install = {},
-
-    highlight = { enable = true },
-    indent = {
-      enable = true,
-      disable = { "groovy", "python", "xml", }
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "]<space>",
-        node_incremental = "]<space>",
-        scope_incremental = "]i",
-        node_decremental = "]d",
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
-        },
-      },
-    },
-  }
-end, 0)
 
 -- [[ Diagnostics ]]
 local diagnostic_opts = {
