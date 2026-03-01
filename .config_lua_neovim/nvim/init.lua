bf7a0aee50d71dc8a7185c603e02b3c11492075b
Juan Comesaña <jcomesana@gmail.com>
2026-03-01 22:12:31 +0100

    chore(nvim): updated npm-groovy-lint version

--- .config_lua_neovim/nvim/init.lua
+++ .config_lua_neovim/nvim/init.lua
@@ -1488,7 +1488,7 @@ vim.g.rustaceanvim = {
 
 local lsp_servers_handled_with_mason = {}
 if (not is_termux) then
-  table.insert(lsp_servers_handled_with_mason, { "npm-groovy-lint", version = "16.2.0" })
+  table.insert(lsp_servers_handled_with_mason, { "npm-groovy-lint", version = "17.0.0" })
 end
 
 for server_name, server_config in pairs(lsp_servers) do
