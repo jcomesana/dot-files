b61ed2a963b79049ff35a347a289a4751d5fc537
Juan Comesaña <jcomesana@gmail.com>
2026-07-01 10:37:56 +0200

    chore: bump npm-groovy-lint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1604,7 +1604,7 @@ else
 
   local lsp_servers_handled_with_mason = {}
   if (not is_termux) then
-    table.insert(lsp_servers_handled_with_mason, { "npm-groovy-lint", version = "17.0.5" })
+    table.insert(lsp_servers_handled_with_mason, { "npm-groovy-lint", version = "18.0.0" })
   end
 
   for server_name, server_config in pairs(lsp_servers) do
